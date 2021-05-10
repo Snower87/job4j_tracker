e180cfea24594cca9ac112f35b6b176ea2d2fc43
Sergei Begletsov <snower87@yandex.ru>
2021-05-10 18:25:52 +0500

    1.1. Создание объекта. [165452#271542]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+/**
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 1.1. Создание объекта. [165452#271542]
+ * @Описание 1. Создайте класс ru.job4j.oop.Cat.
+ * 2. Попрактикуйтесь создавать объекты в методе main() класса Cat (из примера к задаче).
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 10.05.2021
+ * @version 1
+ */
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+/**
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 1.1. Создание объекта. [165452#271542]
+ * @Описание 1. Создайте класс ru.job4j.oop.Dog.
+ * 2. Добавьте в него метод main().
+ * 3. В методе main создайте три объекта класса Dog. Присвойте им имена polkan, sharik, zhychka.
+ * 4. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 10.05.2021
+ * @version 1
+ */
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
