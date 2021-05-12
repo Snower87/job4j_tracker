b35cbdfb133bc3e587bae4639f826c9e7c1c0c50
Sergei Begletsov <snower87@yandex.ru>
2021-05-12 13:12:56 +0500

    1.3. Вызов метода с аргументами. [166086#271545]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,14 +2,20 @@ package ru.job4j.oop;
 
 /**
  * @Раздел Блок 2. ООП / 1. Классы и объекты
- * @Задание 1.2. Вызов метода объекта. [166085#271544]
+ * @Задание 1.2. Вызов метода объекта. [166085#271544] (ver.1)
  * @Описание 1. Создайте класс ru.job4j.oop.Student. Добавьте в него метод public void song().
  * Этот метод должен выводить на консоль слова "I believe I can fly".
  * 2. В методе main() попросите Петю три раза сыграть на баяне и три раза спеть.
  * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ *
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 1.3. Вызов метода с аргументами. [166086#271545] (ver.2)
+ * @Описание 1. Доработал класс, добавив в него переогруженный метод public void music(String lyrics) (по описанию к задаче).
+ * 2. В методе main() вызовите новый метод с выводом на консоль.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
  * @author Sergei Begletsov
  * @since 12.05.2021
- * @version 1
+ * @version 2
  */
 
 public class Student {
@@ -19,6 +25,14 @@ public class Student {
      */
     public void music() {
         System.out.println("Tra tra tra");
+    }
+
+    /**
+     * Перегрузка метода со входным пар-ом (строкой)
+     * @return вывод на консоль строки
+     */
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 
     /**
@@ -38,5 +52,7 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
     }
 }
