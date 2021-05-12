810a908f47a87e990e618a4a4b92c53b73ff763f
Sergei Begletsov <snower87@yandex.ru>
2021-05-12 13:43:49 +0500

    1.4. Вызов метода с возвращаемым типом. [167387#271547]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,18 +2,34 @@ package ru.job4j.oop;
 
 /**
  * @Раздел Блок 2. ООП / 1. Классы и объекты
- * @Задание 1.1. Создание объекта. [165452#271542]
+ * @Задание 1.1. Создание объекта. [165452#271542] (ver.1)
  * @Описание 1. Создайте класс ru.job4j.oop.Cat.
  * 2. Попрактикуйтесь создавать объекты в методе main() класса Cat (из примера к задаче).
  * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ *
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 1.4. Вызов метода с возвращаемым типом. [167387#271547] (ver.2)
+ * @Описание 1. Практика работы с методами, возвращающими значение (сделал по описанию к задаче).
  * @author Sergei Begletsov
  * @since 10.05.2021
- * @version 1
+ * @version 2
  */
 
 public class Cat {
+    /**
+     * Возвращает голос кошки
+     * @return String "may-may" для объекта типа Cat
+     */
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
 }
---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+/**
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 1.4. Вызов метода с возвращаемым типом. [167387#271547]
+ * @Описание 1. Создайте класс ru.job4j.oop.DummyDic. Создайте в нем метод public String engToRus(String eng)
+ * 2. Метод должен вернуть строчку "Неизвестное слово. " + eng.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 12.05.2021
+ * @version 1
+ */
+
+public class DummyDic {
+    /**
+     * Переводит слово с англ. -> русский
+     * @param eng новое слово для словаря
+     * @return возвращает строку "Неизвестное слово. " + eng
+     */
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng ;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dictonary = new DummyDic();
+        System.out.println(dictonary.engToRus("apple"));
+        System.out.println(dictonary.engToRus("object"));
+        System.out.println(dictonary.engToRus("link"));
+    }
+}
