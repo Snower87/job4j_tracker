2163ff812958011aae03687a4d0522ef3d0834f2
Sergei Begletsov <snower87@yandex.ru>
2021-05-14 12:14:38 +0500

    5.1. Зона видимости переменных. [180147#271549]

---
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -0,0 +1,34 @@
+package ru.job4j.max;
+
+/**
+ * @Раздел Блок 2. ООП / 1. Классы и объекты
+ * @Задание 5.1. Зона видимости переменных. [180147#271549]
+ * @Описание 1. Код ниже содержит ошибку. Код должен вывести на экран массив, который мы передали в метод to(...).
+ * Метод to(..) должен записать аргумент array в поле класса.
+ * 2. Исправьте ошибку.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 14.05.2021
+ * @version 1
+ */
+
+public class Reduce {
+    private int[] array;
+
+    public void to(int[] array) {
+        this.array = array; //<<-- здесь была ошибка
+    }
+
+    public void print() {
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        Reduce reduce = new Reduce();
+        reduce.to(array);
+        reduce.print();
+    }
+}
