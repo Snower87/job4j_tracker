bfb85a74029cef3cfaeac450c738c7bb56bf119b
Sergei Begletsov <snower87@yandex.ru>
2021-05-17 12:12:40 +0500

    2. Перегрузка конструктора. [173890#271554]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -2,12 +2,17 @@ package ru.job4j.oop;
 
 /**
  * @Раздел Блок 2. ООП / 2. Наследование
- * @Задание 1. Конструктор по умолчанию. [173889#271553]
+ * @Задание 1. Конструктор по умолчанию. [173889#271553] (ver.1)
  * @Описание 1. Практика создания объектов, конкструкторов с параметрами и по-умолчанию (по описанию к задаче)
+ * 2. Загрузите код в github. Оставьте ссылку на коммит.
+ *
+ * @Раздел Блок 2. ООП / 2. Наследование
+ * @Задание 2. Перегрузка конструктора. [173890#271554] (ver.2)
+ * @Описание 1. Практика перегрузки конструкторов класса (по описанию к задаче)
  * 2. Загрузите код в github. Оставьте ссылку на коммит.
  * @author Sergei Begletsov
  * @since 16.05.2021
- * @version 1
+ * @version 2
  */
 
 public class Computer {
@@ -20,8 +25,14 @@ public class Computer {
     public Computer() {
     }
 
-    public Computer(boolean multiMonitor, int ssd, String cpu) {
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
         this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    //перегрузка конструктора
+    public Computer(int ssd, String cpu) {
         this.ssd = ssd;
         this.cpu = cpu;
     }
@@ -41,5 +52,9 @@ public class Computer {
         compRyzen5.printInfo();
         Computer comp3 = new Computer();
         comp3.printInfo();
+        Computer fourth = new Computer(240, "AMD Ryzen 5 3600");
+        fourth.printInfo();
+        Computer five = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        five.printInfo();
     }
 }
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,39 @@
 package ru.job4j.tracker;
 
+/**
+ * @Раздел Блок 2. ООП / 2. Наследование
+ * @Задание 2. Перегрузка конструктора. [173890#271554]
+ * @Описание 1. В классе ru.job4j.tracker.Item, который у Вас уже есть в проекте Tracker, должно быть два поля:
+ * - int id;
+ * - String name.
+ * Если какого-то поля не хватает, то добавьте его.
+ * 2. Создайте три конструктора: без параметров, с параметром String name, с параметрами int id и String name.
+ * Конструкторы с параметрами должны инициализировать поля, которые объявлены в классе.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 17.05.2021
+ * @version 1
+ */
+
 public class Item {
+    //1. Поля
     private int id;
     private String name;
 
+    //2. Конструкторы
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //3. Методы
     public int getId() {
         return id;
     }
