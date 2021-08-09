332615a5ab454b7c90f48b0c8d378517731a1f9a
Sergei Begletsov <snower87@yandex.ru>
2021-08-09 17:49:10 +0500

    2. Очередь с приоритетом на LinkedList. [#41670 #23673]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,45 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+/**
+ * @Раздел Блок 3. Collections. Lite / 1. Коллекция List, ArrayList
+ * @Задание 2. Очередь с приоритетом на LinkedList. [#41670 #23673]
+ * @Описание 1. Реализуйте метод put, который должен вставлять в нужную позицию элемент в зависимости от приоритета
+ * в классе PriorityQueue.
+ * 2. Очередь с приоритетами - это коллекция, в которой элементы при добавлении располагаются в определенном порядке.
+ * Это позволяет их извлекать в нужном порядке. Например. У нас есть список задач на день с указанием их важности.
+ * Мы бы хотели выполнять задачи по их важности.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 09.08.2021
+ * @version 1
+ */
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определяется по полю приоритет.
+     * Для вставки использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() <= task.getPriority()) {
+                index++;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    /**
+     * Метод возвращает первый элемент из списка и удаляет его
+     * @return возвращает первый элемент
+     */
+    public Task take() {
+        return tasks.poll();
+    }
+}
---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenEqualsPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("first", 5));
+        queue.put(new Task("second", 5));
+        queue.put(new Task("third", 5));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("first"));
+    }
+}
