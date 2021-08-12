75d9a522886d08b106cd4d9823435e2a9abc4d8e
Sergei Begletsov <snower87@yandex.ru>
2021-08-12 19:27:42 +0500

    1. Паспорт и жители. [#212656 #23688]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ /* Класс PassportOffice сервис для работы Паспортного стола 1) добавляет пользователей (жителей), и получает их
+    по номеру паспорта (#90)
+ * @author Sergei Begletsov
+ * @since 12.08.2021
+ * @version 1
+ */
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    /**
+     * Метод добавляет пользователей в структуру данных
+     * @param citizen новый пользователель (житель)
+     * @return true - пользователь не найден, значит добавляем его,
+     *         false - пользователь найден, не добавляем его
+     */
+    public boolean add(Citizen citizen) {
+        boolean rsl = false; //новый пользователь найден
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет существует ли пользователь с таким паспортом
+     * @param passport паспорт пользователя
+     * @return если существует - то возвращает его,
+     *         нет - то возвращает null.
+     */
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+    @Test
+    public void add1Сitizen() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void add1CitizensAndTestTrue() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        assertTrue(office.add(citizen));
+    }
+
+    @Test
+    public void add2EqualsCitizensAndTestFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizen));
+    }
+}
