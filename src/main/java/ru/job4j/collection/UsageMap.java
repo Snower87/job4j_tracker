28f17dd7f5d6e534eaff3c1f5cb36026a71ed481
Sergei Begletsov <snower87@yandex.ru>
2021-08-12 18:59:14 +0500

    0. Map, HashMap. [#212652 #23687]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ /* Класс UsageMap практика работы с коллекциями HashMap (#89)
+ * @author Sergei Begletsov
+ * @since 12.08.2021
+ * @version 1
+ */
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("sergeibezk@yandex.ru", "Sergei Bezkoritov");
+        map.put("vovapopov@yandex.ru", "Vladimir Popov");
+        map.put("ivnpoddubnii@yandex.ru", "Ivan Poddubnii");
+        map.put("parsentev@yandex.ru", "Petr Arsentev2");
+
+        //Вариант №1. Итерирование и вывод через keySet
+        for (String key: map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+        System.out.println();
+
+        //Вариант №2. Итерирование и вывод через метод entrySet()
+        map.remove("parsentev@yandex.ru"); //удаление ключа
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+        System.out.println();
+
+        //Создание коллекции HashMap:
+        //почта - ключ, ФИО - значение
+        HashMap<String, String> postAndName = new HashMap<>();
+        postAndName.put("aaa@yandex.ru", "Халкаев Азамир Азизович");
+        postAndName.put("about@mail.ru", "mail.ru");
+        postAndName.put("petrov@uber.ru", "Петров Иван Сергеевич");
+        postAndName.put("amazon@hotmail.ru", "Bernanke Fiezeph Knauht");
+
+        for (String key: postAndName.keySet()) {
+            System.out.println(postAndName.get(key));
+        }
+    }
+}
