13deb8a392a21c4fc1e9a74035c5393e3de31c08
Sergei Begletsov <snower87@yandex.ru>
2021-08-31 15:51:41 +0500

    3. Уникальность элементов и сортировка. [#110225 #23506]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,14 +1,15 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Класс-сервис Profile содержит профили всех клиентов
- * 1) создание класса (#117)
+ * 1) создание класса (#117) 2) добавил метода collectUnique - собирает уникальные адреса (#118)
  * @author Sergei Begletsov
  * @since 31.08.2021
- * @version 1
+ * @version 2
  */
 
 public class Profiles {
@@ -21,5 +22,33 @@ public class Profiles {
         return profiles.stream()
                             .map(profile -> profile.getAddress())
                             .collect(Collectors.toList());
+    }
+
+    /**
+     * Получить список уникальных адресов всех клиентов
+     * @param profiles входной список профилей клиентов (с адресами)
+     * @return список адресов всех клиентов
+     */
+    public static List<Address> collectUnique(List<Profile> profiles) {
+        return profiles.stream()
+                //1 Вариант (мой первоначальный)
+                /*
+                .sorted(Comparator.comparing(profile -> profile.getAddress().getCity()))
+                .distinct()
+                .map(el -> el.getAddress())
+                .collect(Collectors.toList());
+                */
+                //2 Вариант (предложение от Стаса)
+                /*
+                .map(Profile::getAddress)
+                .sorted((p1, p2) -> p1.getAddress().getCity().compareTo(p2.getAddress().getCity()))
+                .distinct()
+                .collect(Collectors.toList());
+                */
+                //3 Вариант
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -41,4 +41,108 @@ public class ProfilesTest {
         //3. Выдать ошибку, если не равны
         assertEquals(expectedAddress, Profiles.collect(profiles));
     }
+
+    @Test
+    public void whenCollect1UniqueAddrFromListWith3Profiles() {
+        //1. Формирование входных значений
+        List<Profile> profileList = new ArrayList<>();
+        Address addrVasia = new Address("Saint-Peterburg","Morskaya",14, 35);
+        Profile vasia = new Profile(addrVasia);
+        profileList.add(vasia);
+        profileList.add(vasia);
+        profileList.add(vasia);
+
+        //2. Ожидаемый список адресов на выходе
+        List<Address> expectedAddress = Arrays.asList(
+                addrVasia
+        );
+
+        //3. Выдать ошибку, если не равны
+        assertEquals(expectedAddress, Profiles.collectUnique(profileList));
+    }
+
+    @Test
+    public void whenCollect2UniqueAddrAndSortedItFromListWith4Profiles() {
+        //1. Формирование входных значений
+        List<Profile> profileList = new ArrayList<>();
+        Address addrVasia = new Address("Saint-Peterburg","Morskaya",14, 35);
+        Profile vasia = new Profile(addrVasia);
+        profileList.add(vasia);
+        Address addrPetya = new Address("Dmitriev","Gagarina",15, 31);
+        Profile petya = new Profile(addrPetya);
+        profileList.add(petya);
+        profileList.add(vasia);
+        profileList.add(petya);
+
+        //2. Ожидаемый список адресов на выходе
+        List<Address> expectedAddress = Arrays.asList(
+                addrPetya,
+                addrVasia
+        );
+
+        //3. Выдать ошибку, если не равны
+        assertEquals(expectedAddress, Profiles.collectUnique(profileList));
+    }
+
+    @Test
+    public void whenCollect5UniqueAddrFromListWith5Profiles() {
+        //1. Формирование входных значений
+        List<Profile> profileList2 = new ArrayList<>();
+        Address addrVasia = new Address("Saint-Peterburg","Morskaya",14, 35);
+        Profile vasia = new Profile(addrVasia);
+        profileList2.add(vasia);
+        Address addrSergey = new Address("Omsk","Lenina",13, 34);
+        Profile sergey = new Profile(addrSergey);
+        profileList2.add(sergey);
+        Address addrDima = new Address("Petrozavodsk","Ilicha",62, 33);
+        Profile dima = new Profile(addrDima);
+        profileList2.add(dima);
+        Address addrMarat = new Address("Vitebsk","Portnaya",94, 32);
+        Profile marat = new Profile(addrMarat);
+        profileList2.add(marat);
+        Address addrPetya = new Address("Dmitriev","Gagarina",15, 31);
+        Profile petya = new Profile(addrPetya);
+        profileList2.add(petya);
+
+        //2. Ожидаемый список адресов на выходе
+        List<Address> expectedAddress = Arrays.asList(
+                addrPetya,
+                addrSergey,
+                addrDima,
+                addrVasia,
+                addrMarat
+        );
+
+        //3. Выдать ошибку, если не равны
+        assertEquals(expectedAddress, Profiles.collectUnique(profileList2));
+    }
+
+    @Test
+    public void whenCollect3UniqueAddrFromListWith7Profiles() {
+        //1. Формирование входных значений
+        List<Profile> profileList2 = new ArrayList<>();
+        Address addrVasia = new Address("Saint-Peterburg","Morskaya",14, 35);
+        Profile vasia = new Profile(addrVasia);
+        profileList2.add(vasia);
+        Address addrSergey = new Address("Omsk","Lenina",13, 34);
+        Profile sergey = new Profile(addrSergey);
+        profileList2.add(sergey);
+        Address addrDima = new Address("Petrozavodsk","Ilicha",62, 33);
+        Profile dima = new Profile(addrDima);
+        profileList2.add(dima);
+        profileList2.add(vasia);
+        profileList2.add(vasia);
+        profileList2.add(sergey);
+        profileList2.add(dima);
+
+        //2. Ожидаемый список адресов на выходе
+        List<Address> expectedAddress = Arrays.asList(
+                addrSergey,
+                addrDima,
+                addrVasia
+        );
+
+        //3. Выдать ошибку, если не равны
+        assertEquals(expectedAddress, Profiles.collectUnique(profileList2));
+    }
 }
