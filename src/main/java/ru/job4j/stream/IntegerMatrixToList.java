435c510c1955f9bfe7077b8e83ce6d051ae9832f
Sergei Begletsov <snower87@yandex.ru>
2021-09-05 14:24:10 +0500

    Устранение ошибок в п.5. Преобразование матрицы чисел в список чисел. [#110228 #23508]

--- src/main/java/ru/job4j/stream/IntegerMatrixToList.java
+++ src/main/java/ru/job4j/stream/IntegerMatrixToList.java
@@ -3,13 +3,14 @@ package ru.job4j.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Класс IntegerMatrixToList преобразует массив Integer[][] в список List
- * 1) создание класса (#121)
+ * 1) создание класса (#121) 2) переход с Arrays.stream(array) -> Stream.of(array)
  * @author Sergei Begletsov
  * @since 04.09.2021
- * @version 1
+ * @version 2
  */
 
 public class IntegerMatrixToList {
@@ -19,8 +20,13 @@ public class IntegerMatrixToList {
      * @return список чисел Integer
      */
     public static List<Integer> convert(Integer[][] matrix) {
-        return Arrays.stream(matrix)
-                    .flatMap(e -> Arrays.stream(e))
-                    .collect(Collectors.toList());
+        //Вариант №1
+        //return Arrays.stream(matrix)
+        //        .flatMap(Stream::of)
+        //        .collect(Collectors.toList());
+        //Вариант №2
+        return Stream.of(matrix)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
     }
 }
