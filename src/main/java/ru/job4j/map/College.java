728adf77e99b6deebdc991ba190ae40229fe0aa7
Sergei Begletsov <snower87@yandex.ru>
2021-11-02 17:01:11 +0500

    3. Optional в банковских переводах. [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,14 +1,16 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 /**
  * Класс-сервис College
- * 1) создание класса (#124)
+ * 1) создание класса (#124) 2) добавил методы findByAccount, findBySubjectName, реализовав их через
+ * оболочку Optional, чтобы избавиться от ошибки NPE (#139)
  * @author Sergei Begletsov
  * @since 07.09.2021
- * @version 1
+ * @version 2
  */
 
 public class College {
@@ -23,7 +25,7 @@ public class College {
      * @param account аккаунт студента
      * @return возвращает найденного студента или null
      */
-    public Student findByAccount(String account) {
+    public Student findByAccount1(String account) {
         return students.keySet().stream()
                 .filter(student -> student.getAccount().equals(account))
                 .findFirst()
@@ -36,8 +38,8 @@ public class College {
      * @param name имени предмета
      * @return возвращает найденного предмет или null
      */
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
+    public Subject findBySubjectName1(String account, String name) {
+        Student a = findByAccount1(account);
         if (a != null) {
             return students.get(a).stream()
                     .filter(subject -> subject.getName().equals(name))
@@ -45,6 +47,43 @@ public class College {
                     .orElse(null);
         }
         return null;
+    }
+
+    /**
+     * Поиска студента по аккаунту (через Optional)
+     * @param account аккаунт студента
+     * @return Optional из студента или null
+     */
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s: students.keySet()) {
+            if (account.equals(s.getAccount())) {
+                rsl = Optional.of(s);
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Поиск предмета по аккаунту и имени предмета (через Optional)
+     * @param account аккаунт студента
+     * @param name имени предмета
+     * @return Optional из предмета или null
+     */
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> rsl = Optional.empty();
+        Optional<Student> s = findByAccount(account);
+        if (s.isPresent()) {
+            Set<Subject> subjects = students.get(s.get());
+            for (Subject subj : subjects) {
+                if (name.equals(subj.getName())) {
+                    rsl = Optional.of(subj);
+                    break;
+                }
+            }
+        }
+        return rsl;
     }
 
     public static void main(String[] args) {
@@ -55,9 +94,9 @@ public class College {
                 )
         );
         College college = new College(students);
-        Student student = college.findByAccount("000001");
+        Student student = college.findByAccount1("000001");
         System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
+        Subject english = college.findBySubjectName1("000001", "English");
         System.out.println("Оценка по найденному предмету: " + english.getScore());
     }
 }
